ce201e3439fb7871265e7dec061d8c00e2619066
Carlos Barajas <CBarajas002@gmail.com>
2021-09-10 20:41:23 -0700

    In javaScript added when clicked on continue the test questions pops up, fixed some html, and add the action to CSS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var start_btn = document.querySelector(".start_btn button");
 var info_box = document.querySelector(".info_box");
 var exit_btn = info_box.querySelector(".buttons .quit");
-var continue_btn = info_box.querySelector(".button .restart");
-var quiz_box = info_box.querySelector(".quiz_box");
+var continue_btn = info_box.querySelector(".buttons .restart");
+var quiz_box = document.querySelector(".quiz_box");
 
 // If Start Quiz button clicked 
 start_btn.onclick = ()=>{
@@ -15,8 +15,7 @@ exit_btn.onclick = ()=>{
 }
 
 
-// If Continue button clicked 
-exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo");
-    quiz_box.classList.remove("activeQuiz"); //Shows the quiz box
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo")
+    quiz_box.classList.add("activeQuiz"); //Shows the quiz box
 }
