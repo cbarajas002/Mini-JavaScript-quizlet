8bbb9d43ae584a8c70a9fd4b7ea1fe05bba2d49d
Carlos Barajas <CBarajas002@gmail.com>
2021-09-11 19:48:35 -0700

    finished all the questions now need to connet it to my html so I can pull to my JavaScript

---
+++ questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is JavaScript?",
+        answer: "JavaScript is a client-side and server-side scripting language",
+        options: [
+            "JavaScript is a client-side and server-side scripting language",
+            "JavaScript is a smart AI",
+            "JavaScript is a web brower",
+            "none of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What are JavaScript Data Types?",
+        answer: "All of the above",
+        options: [
+            "Object",
+            "Number",
+            "String",
+            "All of the above"
+        ]
+    }, 
+    {
+        numb: 3,
+        question: "Which company developed JavaScript?",
+        answer: "Netscape",
+        options: [
+            "Apple",
+            "microsoft",
+            "Google",
+            "Netscape"
+        ]
+    }, 
+    {
+        numb: 4,
+        question: "What are all the types of Pop up boxes available in JavaScript?",
+        answer: "All of the above",
+        options: [
+            "Alert",
+            "Confirm ",
+            "Prompt",
+            "All of the above"
+        ]
+    }, 
+    {
+        numb: 5,
+        question: "What are all the looping structures in JavaScript?",
+        answer: "All of the above",
+        options: [
+            "For",
+            "While",
+            "Do-while loops",
+            "All of the above"
+        ]
+    }, 
+
+   
+
+];
